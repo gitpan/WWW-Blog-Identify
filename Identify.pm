e2a567db041fdcc67833e25bde9118a422c4bd2e
Maciej Ceglowski <mceglows@middlebury.edu>
2003-05-23 12:39:28 +0000

    Import of MCEGLOWS/WWW-Blog-Identify-0.03 from CPAN.
    gitpan-cpan-distribution: WWW-Blog-Identify
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MCEGLOWS/WWW-Blog-Identify-0.03.tar.gz
    gitpan-cpan-author:       MCEGLOWS
    gitpan-cpan-maturity:     released

--- Identify.pm
+++ Identify.pm
@@ -9,11 +9,11 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw/identify/;
 
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 sub identify {
-	my ($url, $text) = @_;
+	my ($url, $text) = @_; 
 	
 	$url = lc( $url ); 
 	local $_ = $url;
@@ -28,7 +28,7 @@ sub identify {
 	return "blogspot" if /\.blogspot\.com/o;
 	return "blogger" if m|\.blogger\.com/|o;
 	
-	return "blogger (br)" if  m|\.blogger\.com\.br/|o; # Brazilian Blogger
+	return "blogger (br)" if  m|\.blogger\.com\.br|o; # Brazilian Blogger
 	return 'terra' if  m|weblogger\.(terra\.)?com\.br/|o;
 	return "diaryland" if  /\.diaryland\./o;
 	return "livejournal" if  /\.livejournal\.com/o;
@@ -36,7 +36,7 @@ sub identify {
 	return "blogalia" if  /\.blogalia\.com/o;
 	return "pitas" if  /\.pitas\.com/o;
 	return "persianblog" if  /\.persianblog\.com/o; # Farsi
-	return "persianlog" if  /\.persianlog\.com/o;  # Farsi
+	return "persianlog" if  /\bpersianlog\.com/o;  # Farsi
 	
 	return "manila" if  /\.manilasites\.com/o;
 	return "manila" if  /\.editthispage\.com/o;
@@ -44,6 +44,9 @@ sub identify {
 	return "manila" if  m|\.weblogs\.com/|o;
 	
 	return "radio" if  /\radio.weblogs\.com/o;
+	return "radio" if /blogs.law.harvard.edu/o;
+	return "radio" if /\.blogs.it\b/o;
+	
 	return "twoday" if  /\.twoday\.net/o;
 	return "salon" if  /blogs\.salon\.com/o;
 	return "splinder" if  /\.splinder\.it/o;		# Italy
@@ -51,7 +54,11 @@ sub identify {
 	return "antville" if  /\.antville\.org/o;
 	return 'bloggingnetwork' if  m|\.bloggingnetwork\.com/blogs|o;
 	return "crimsonblog" if  /\.crimsonblog\./o;
+	
 	return "blog.pl (polish)" if  /\.blog\.pl/o;
+	return "e-blog.pl (polish)" if  /\.e-blog\.pl/o;
+	return "weblog.pl (polish)" if  /\.weblog\.pl/o;
+	
 	return "twoday" if  /\.twoday\.net/o;
 	return "monblogue" if  /\.monblogue\.com/o;
 	return 'joueb' if  m|joueb\.com/|o;				# France
@@ -59,8 +66,13 @@ sub identify {
 	return 'blog-city' if  m|blog-city\.com/|o;
 	return 'blogsky' if  m|\.blogsky\.com/|o;		# English and Persian
 	return 'u-blog' if  m|u-blog\.net/|o;	 		# France
-	return 'barrapunto' if  m|\.barrapunto\.com/index\.pl|o;	# Spain
-	return 'blig' if  m|\.blig\.(?:ig.)?com\.br/|o;	# Brazil
+	return 'barrapunto' if  m|\bbarrapunto\.com/index\.pl|o;	# Spain
+	return 'blig' if  m|\.blig\.(?:ig.)?com\.br|o;	# Brazil
+	return 'g-blog' if  m|g-blog\.net/|o;
+	return 'babelogue' if m|babelogue\.citypages\.com|io;
+	return 'jevon' if m|\.jevon\.org/|io;
+	return 'tripod' if m|\.tripod\.com/|io;
+	
 	#
 	#	 CONTENT CHECKING
 	#
@@ -71,27 +83,31 @@ sub identify {
 	
 	return "postnuke" if m|CONTENT="Post-?Nuke|io;	# Nuke is nice enough to use META tags
 	return "php-nuke" if m|CONTENT="PHP-?Nuke|io;
-	return "microsoft" if m|name="GENERATOR" Content="Microsoft Visual|io;
-	return "nucleus" if m|name="generator" content="Nucleus|io;
-
+	return "microsoft" if m|<meta[^>]+Content=['"]Microsoft Visual|io;
+	return "nucleus" if m|<meta[^>]+content=['"]Nucleus|io;
+	return "greymatter" if m|<meta[^>]+content=['"]Greymatter|io;
+	return "land down under" if m|<meta[^>]+content=['"]Land Down Under|io;
+	
 	# Next, check actual content
 	
 	return "movable type" if m|cgi-bin/mt|o;
-	return "movable type" if m|Powered by.*Move?able Type|io; # common typo is 'Moveable'
+	return "movable type" if m|Powered by.*Move?able ?Type|io; # common typo is 'Moveable'
 	return "movable type" if m|mtblog.gif|io;
 	return "movable type" if m|move?abletype.gif|o;
 	return "movable type" if m!function Open(Trackback|Comments)\s+\(c\)!o; # default MT JavaScript
 
 	return "blogger pro" if m|powered_by_blogger_pro[0-9]*\.gif|io;
-	return "blogger pro" if m|powered by <a href="[^>]+>blogger pro</a>|io;
-	
-	return "blogger" if m|bloggerbutton[0-9]+.gif|o;
+	return "blogger pro" if m|powered by:? <a href="[^>]+>blogger pro</a>|io;
+		
+	return "blogger" if m|bloggerbutton[0-9]+.gif|io;
+	return "blogger" if m|bloggertemplate[^.]+.gif|io;
 	return "blogger" if m|blogger_bluelong.gif|o;
 	return "blogger" if m|powered by (<a href="[^>]+>)?blogger(</a>)?|io;
 	
 	return "radio" if m|img src="http://radio.weblogs.com|io;
-	return "radio" if m|http://radio.xmlstoragesystem.com/weblogStats|;
-	return "radio" if m|images/radioUserLand|;
+	return "radio" if m|http://radio.xmlstoragesystem.com/weblogStats|oi;
+	return "radio" if m|images/radioUserLand|oi;
+	return "radio" if m|xmlCoffeeCup|oi;
 	
 	return "manila" if m|thisIsAManilaSite|oi;
 	
@@ -111,6 +127,8 @@ sub identify {
 	
 	return "pMachine" if m|alt="[^"]+ pMachine|io; # This can be "Powered by" or "Gemaakt mit", for example
 	return "pMachine" if m|powered by (?:<a href="[^>]+>)?pMachine|io;
+	return "pMachine" if m|pmachine.gif|io;
+	
 	return "psychoblogger" if m|Powered by (?:<a [^>]+>)?Psychoblogger|io;
 	
 	
@@ -118,6 +136,7 @@ sub identify {
 	my @blog_count = $text =~ /\bblog\b/gi;
 	
 	return "suspected by URL" if $url =~ /[\W\-_](?:we)?blog/o;
+	return "suspected by URL" if $url =~ /\bbitacoras\b/i;
 	return "suspected by rss" if $text =~ /\brss\b/i;
 	return "suspected by content" if scalar @blog_count > 5;
 	
